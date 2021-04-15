095b3b0481880fe9c9a306bb576779fbe420028d
Smriti <smritipillai.smriti@gmail.com>
2021-04-15 23:16:16 +0530

    changes

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,16 +1,16 @@
 #include "inc/header.h"
 
-void playlist_menu(void);
+void playlist_menu(int);
 
 //Start of the application
 int main(int argc, char *argv[]) {
-    int n=0;
+    int n=0,r=0;
     while(n!=6) {
-        playlist_menu();
+        playlist_menu(r);
     }
 }
 
-void playlist_menu(void) {
+void playlist_menu(int r) {
     int n;
     printf("\n 1. Add song to the playlist\n");
     printf("\n 2. Display songs available on the playlist\n");
@@ -19,7 +19,8 @@ void playlist_menu(void) {
     printf("\n 5. List the count of songs available\n");
     printf("\n 6. Exit the system\n");
     printf("\n Enter one of the options from above: \n");
-    scanf("%d", &n);
+    if(r==0)
+        scanf("%d", &n);
     switch(n) {
         case 1:  //Add new song
             addSong();
--- 3_Implementation/test/test_playlist_operations.c
+++ 3_Implementation/test/test_playlist_operations.c
@@ -11,32 +11,9 @@ void tearDown()
 {
 }
 
-// FILE *f = NULL;
-// f = fopen("playlist.txt", "w+");
-char songName[9] = "Stranger";
-char Fname[4] = "Sam";
-char Lname[6] = "Smith";
-char G[1] = "M";
-char Genre[1] = "H";
-int stream = 0;
-
-void test_addSong(void){
-    TEST_ASSERT_EQUAL(0, check(songName, "Stranger"));
-    TEST_ASSERT_EQUAL(0, check(Fname, "Sam"));
-    TEST_ASSERT_EQUAL(0, check(Lname, "Smith"));
-    TEST_ASSERT_EQUAL(0, check(G, "M"));
-    TEST_ASSERT_EQUAL(0, check(Genre, "H"));
-    TEST_ASSERT_EQUAL(0, check(stream, 0));
+void test_menu(){
+    TEST_ASSERT_EQUAL(0, playlist_menu(0));
 }
-
-// void test_songsByGenre(void){
-//     TEST_ASSERT_EQUAL(0, check(Genre, "M"));
-// }
-
-// void test_songsByArtist(void){
-//     TEST_ASSERT_EQUAL(0, check(Fname, "Sam"));
-//     TEST_ASSERT_EQUAL(0, check(Lname, "Smith"));
-// }
 
 int main(void)
 {
@@ -44,10 +21,7 @@ int main(void)
     UNITY_BEGIN();
 
     /* Run Test functions */
-    //RUN_TEST(test_fileOpen);
-     RUN_TEST(test_addSong);
-    // RUN_TEST(test_songsByGenre);
-    // RUN_TEST(test_songsByArtist);
+    RUN_TEST(test_menu);
     
     /* Close the Unity Test Framework */
     return UNITY_END();
