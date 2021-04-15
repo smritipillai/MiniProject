c91b9e45fb1a6b8988d04f289e9e3ab6a334daab
Smriti <smritipillai.smriti@gmail.com>
2021-04-15 21:08:54 +0530

    test updated

--- 3_Implementation/test/test_playlist_operations.c
+++ 3_Implementation/test/test_playlist_operations.c
@@ -1,5 +1,6 @@
 #include "../inc/header.h"
 #include "../unity/unity.h"
+#include "../unity/unity_internals.h"
 
 /* Required by the unity test framework */
 void setUp()
@@ -10,22 +11,41 @@ void tearDown()
 {
 }
 
-char songName[100];
-char f_artist[50];
-char l_artist[50];
-char gender[1];
-char genre[1];
-int platform;
-
 FILE *f = NULL;
+char songName[6] = "Stranger";
+char Fname[4] = "Sam";
+char Lname[6] = "Smith";
+char G[1] = "M";
+char Genre[1] = "H";
+int stream = 0;
+fseek(f,0,SEEK_END);
+fwrite(songName,1,sizeof(songName),f);
+fwrite(Fname,1,sizeof(Fname),f);
+fwrite(Lname,1,sizeof(Lname),f);
+fwrite(G,1,sizeof(G),f);
+fwrite(Genre,1,sizeof(Genre),f);
+fwrite(stream,1,sizeof(stream),f);
 
 void test_fileOpen(void) {
-    TEST_ASSERT_EQUAL(SUCCESS, openFile("playlist.txt"))
+    TEST_ASSERT_EQUAL(0, openFile("playlist.txt", "rb+", &f))
 }
 
-void check_file_display(void){
-        
-        TEST_ASSERT_EQUAL(SUCCESS, readFile(&r));
+void test_addSong(void){
+    TEST_ASSERT_EQUAL(0, check(songName, "Stranger"));
+    TEST_ASSERT_EQUAL(0, check(Fname, "Sam"));
+    TEST_ASSERT_EQUAL(0, check(Lname, "Smith"));
+    TEST_ASSERT_EQUAL(0, check(G, "M"));
+    TEST_ASSERT_EQUAL(0, check(Genre, "H"));
+    TEST_ASSERT_EQUAL(0, check(stream, 0));
+}
+
+void test_songsByGenre(void){
+    TEST_ASSERT_EQUAL(0, check(genre, "M"));
+}
+
+void test_songsByArtist(void){
+    TEST_ASSERT_EQUAL(0, check(Fname, "Sam"));
+    TEST_ASSERT_EQUAL(0, check(Lname, "Smith"));
 }
 
 int main(void)
@@ -35,18 +55,9 @@ int main(void)
 
     /* Run Test functions */
     RUN_TEST(test_fileOpen);
-    RUN_TEST(test_create);
-    RUN_TEST(test_insert);
-    RUN_TEST(check_idx_file_insertion);
-    RUN_TEST(check_file_save);
-    RUN_TEST(check_fileDisplay);
-    RUN_TEST(check_find_by_id);
-    RUN_TEST(check_update_record);
-    RUN_TEST(check_delete_node);
-    RUN_TEST(check_delete_rec_file);
-    RUN_TEST(check_delete_idx_file);
-    RUN_TEST(check_uniq_id);
-    RUN_TEST(check_data_loaded_write);
+    RUN_TEST(test_addSong);
+    RUN_TEST(test_songsByGenre);
+    RUN_TEST(test_songsByArtist);
     
     /* Close the Unity Test Framework */
     return UNITY_END();
